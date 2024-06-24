ff4ffe8f8955bda56dee46a164330cf26d62ebbb
Aulias135 <22611144@students.uii.ac.id>
2024-06-24 15:49:40 +0700

    Update hello.R

--- R/hello.R
+++ R/hello.R
@@ -16,3 +16,23 @@
 hello <- function() {
   print("Hello, world!")
 }
+## Circle Area
+circle = function(phi, r) {
+  circle_area = phi*r^2
+  cat("The area of the circle is :", circle_area,"\n")
+}
+circle(22/7, 14)
+
+## Rectangle Area
+rectangle = function(l, w) {
+  rectangle_area = l * w
+  cat("The area of the rectangle is :", rectangle_area,"\n")
+}
+rectangle(10, 10)
+
+## Triangle Area
+triangle = function(b, h) {
+  triangle_area = 1/2 * b * h
+  cat("The area of the triangle is :", triangle_area,"\n")
+}
+rectangle(2, 5)
